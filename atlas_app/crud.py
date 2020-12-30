1c5c13ed113c3a394da096ed46b7d28629fdb8fe
Dynam1co <asensi.benito@gmail.com>
2020-12-30 18:29:06 +0100

    Deleted item class. Create item model. Create api some methods for item model.

--- atlas_app/crud.py
+++ atlas_app/crud.py
@@ -1,29 +1,37 @@
 """All API methods."""
 
 from sqlalchemy.orm import Session
+from sqlalchemy.sql.sqltypes import Date
 from . import models, schemas
 
 
-def get_user(db: Session, user_id: int):
-    """Return data from a user."""
-    return db.query(models.User).filter(models.User.id == user_id).first()
+def get_item(db: Session, item_id: str):
+    """Return data for a single Item."""
+    return db.query(models.Item).filter(models.Item.id == item_id).first()
 
 
-def get_user_by_email(db: Session, email: str):
-    """Search user by email field."""
-    return db.query(models.User).filter(models.User.email == email).first()
+def get_items(db: Session):
+    """Return data for all Items."""
+    return db.query(models.Item).all()
 
 
-def get_users(db: Session, skip: int = 0, limit: int = 100):
-    """Return data from all users."""
-    return db.query(models.User).offset(skip).limit(limit).all()
+def get_item_by_date(db: Session, dt_insert: Date):
+    """Search items by insertion date."""
+    return db.query(models.Item).filter(models.Item.insert_date == dt_insert).all()
 
 
-def create_user(db: Session, user: schemas.UserCreate):
-    """Insert user into database."""
-    fake_hashed_password = user.password + "notreallyhashed"
-    db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
-    db.add(db_user)
+def create_item(db: Session, item: schemas.ItemBase):
+    """Insert item into database."""
+    db_item = models.Item(
+        id=item.id,
+        media_type=item.media_type,
+        tmdb_id=item.tmdb_id,
+        vote_average=item.vote_average,
+        poster_path=item.poster_path,
+        time_window=item.time_window
+    )
+
+    db.add(db_item)
     db.commit()
-    db.refresh(db_user)
-    return db_user
+    db.refresh(db_item)
+    return db_item
--- atlas_app/models.py
+++ atlas_app/models.py
@@ -1,17 +1,28 @@
 """All models in the database."""
 
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime, Float, Date
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import Index
+from sqlalchemy.dialects.postgresql import UUID
 from .database import Base
+import datetime
+from uuid import uuid4
 
 
-class User(Base):
-    """User Class contains standard information for a User."""
+class Item(Base):
+    """Item object contains standar infrmation for a Movie or TV Show."""
 
-    __tablename__ = "users"
+    __tablename__ = 'item'
 
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
-    is_inactive = Column(Boolean, default=True)
+    id = Column(String, primary_key=True, index=True)
+    media_type = Column(String)
+    insert_datetime = Column(DateTime, default=datetime.datetime.now())
+    insert_date = Column(Date, default=datetime.date.today())
+    tmdb_id = Column(Integer, default=0)
+    published_in_twitter = Column(Boolean, default=False)
+    vote_average = Column(Float, default=0)
+    poster_path = Column(String, default='')
+    title = Column(String, default='')
+    imdb_id = Column(String, default='')
+    overview = Column(String, default='')
+    time_window = Column(String)
