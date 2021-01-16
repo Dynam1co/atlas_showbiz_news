bd41f821d234a6075d343611c33c9730aadad093
Dynam1co <asensi.benito@gmail.com>
2021-01-16 09:54:06 +0100

    Check item exists before insert

--- atlas_app/main.py
+++ atlas_app/main.py
@@ -1,6 +1,7 @@
 """Integrate parts of FastAPI elements."""
 
 from typing import List
+import datetime
 
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
@@ -35,7 +36,12 @@ async def root():
 
 @app.post("/items/", response_model=schemas.Item)
 def create_item(item: schemas.ItemBase, db: Session = Depends(get_db)):
-    """Create Item."""
+    """Create Item."""    
+    db_item = crud.get_item_by_date_and_tmdbid(db, datetime.date.today(), item.tmdb_id)
+
+    if db_item:
+        raise HTTPException(status_code=400, detail="Item already registered")
+
     return crud.create_item(db=db, item=item)
 
 
